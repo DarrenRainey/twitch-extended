268977fa99a96735a3a2d1e7d5a41f1b9513f1f2
twitchextended@gmail.com <twitchextended@gmail.com@35d5faff-760b-8200-1803-82e3d9fd1e5b>
2013-06-25 20:23:37 +0000

    Darer

--- wypok-extended.js
+++ wypok-extended.js
@@ -1,7 +1,7 @@
 $('body').append("<style type='text/css'>"
                       +"#body-con {background: #000000 !important; }"
                       +"#header-con {background: #122532 !important; }"
-                 +".aCC, .bgfa {background: #1E1E1E !important;}"
+                 +".aCC, .bgfa {background: #1A1A1A !important;}"
                  +".alert, .bgfff, .bgf3f3f3 {background: #252525 !important;}"
                       + "</style");
 
