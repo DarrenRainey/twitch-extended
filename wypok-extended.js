759b57498a4a8c7586d94622f125bec8c96f6946
twitchextended@gmail.com <twitchextended@gmail.com@35d5faff-760b-8200-1803-82e3d9fd1e5b>
2013-06-25 20:15:21 +0000

    Komcie

--- wypok-extended.js
+++ wypok-extended.js
@@ -1,5 +1,6 @@
 $('body').append("<style type='text/css'>"
                       +"#body-con {background: #000000 !important; }"
                       +"#header-con {background: #122532 !important; }"
+                      +".aCC, .bgfa {background: #1E1E1E !important;}"
                       + "</style");
 
