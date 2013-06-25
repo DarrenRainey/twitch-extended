86e965998210d65d1cd61f2bf9a2b6c86c3eb822
twitchextended@gmail.com <twitchextended@gmail.com@35d5faff-760b-8200-1803-82e3d9fd1e5b>
2013-06-25 20:07:39 +0000

    00

--- wypok-extended.js
+++ wypok-extended.js
@@ -1,2 +1,5 @@
-$('#body-con').css('background', "#000000 !important");
-$('#header-con').css('background', "#122532");
+jQuery('body').append("<style type='text/css'>"
+                      +"#body-con {background: #000000 !important; }";
+                      +"#header-con {background: #122532 !important; }";
+                      + "</style");
+
