0ce1b5733de39da5ef653b3bc89db8a422aef44b
twitchextended <twitchextended@gmail.com>
2013-06-25 19:46:04 +0000

    sad

--- wypok-extended.js
+++ wypok-extended.js
@@ -1,3 +1,3 @@
 alert($);
-$('#body-con').css('background', "#000000 !important");
+jQuery('#body-con').css('background', "#000000 !important");
 $('#header-con').css('background', "#122532 !important");
